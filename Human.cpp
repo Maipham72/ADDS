7634d1452e71e0b8997886f00538bbb63128a5d9
Maipham72 <hongmai.pham@student.adelaide.edu.au>
2023-08-04 13:59:18 +0930

    add

--- Human.cpp
+++ Human.cpp
@@ -3,6 +3,11 @@
 #include "Rock.h"
 #include "Scissors.h"
 #include "Paper.h"
+#include "Monkey.h"
+#include "Ninja.h"
+#include "Pirate.h"
+#include "Robot.h"
+#include "Zombie.h"
 
 
 Human::Human() {
@@ -26,7 +31,17 @@ Move* Human::makeMove() {
         return new Paper();
     } else if (moveName == "Scissors") {
         return new Scissors();
+    } else if (moveName == "Monkey"){
+        return new Monkey();
+    } else if (moveName == "Ninja") {
+        return new Ninja();
+    } else if (moveName == "Pirate") {
+        return new Pirate();
+    } else if (moveName == "Robot") {
+        return new Robot();
+    } else if (moveName == "Zombie") {
+        return new Zombie();
     } else {
-        return new Rock();
+        return nullptr;
     }
 }
