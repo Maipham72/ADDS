5a1418f5257bde86bcd87592de4f7935dd13bb22
Maipham72 <hongmai.pham@student.adelaide.edu.au>
2023-08-24 09:47:14 +0930

    add

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "Finder.h"
+#include <iostream>
+
+int main () {
+  std::string s1 = "4634554567";
+  std::string s2 = "45689";
+
+  Finder finder;
+  std::vector<int> result = finder.findSubstrings(s1,s2);
+
+  for (int i = 0; i < result.size(); i++) {
+    std::cout << result[i] << " " << std::endl;
+  }
+
+  return 0;
+}
